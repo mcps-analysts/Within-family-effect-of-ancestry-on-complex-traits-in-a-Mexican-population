368f89f897a1f3d032f57ae3fef5abb3e96e99b0
Siqiwsq <105484873+Siqiwsq@users.noreply.github.com>
2026-07-31 22:02:33 +0100

    Format and update REML analysis script

--- 3. REML analysis.sh
+++ 3. REML analysis.sh
@@ -17,12 +17,12 @@ Out_Dir=/path/to/output_directory  # Directory to store results
 
 # Run REML using multiple GRMs
 ${gcta64} \
-  --mgrm mgrm.txt \                            
-  --pheno trait.phen \                          
-  --qcovar quantitative_covariates.txt \        
-  --covar categorical_covariates.txt \          
-  --reml \                                     
-  --reml-est-fix-varcov \                       
-  --reml-no-constrain \                         
-  --thread-num 10 \                            
-  --out ${Out_Dir}/result_name    
+  --mgrm mgrm.txt \
+  --pheno trait.phen \
+  --qcovar quantitative_covariates.txt \
+  --covar categorical_covariates.txt \
+  --reml \
+  --reml-est-fix-varcov \
+  --reml-no-constrain \
+  --thread-num 10 \
+  --out ${Out_Dir}/result_name
